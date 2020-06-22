80449f5080e220d2ffbfcf469066d79d600f475c
Ashigarg123 <tanishkaashi567@gmail.com>
2020-06-22 15:08:07 -0400

    okay

--- static/h7.js
+++ static/h7.js
@@ -6,12 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
   socket.on('message', data => {
     const p = document.createElement('p');
     const span_username = document.createElement('span');
-    //const span_timestamp = document.createElement('span');
+    const span_timestamp = document.createElement('span');
     const br = document.createElement('br');
     if (data.username) {
       span_username.innerHTML = data.username;
-      //span_timestamp.innerHTML = data.time_stamp;
-      p.innerHTML = span_username.outerHTML + br.outerHTML + data.msg + br.outerHTML; //+span_timestamp.outerHTML ;
+      span_timestamp.innerHTML = data.time_stamp;
+      p.innerHTML = span_username.outerHTML + br.outerHTML + data.msg + br.outerHTML; +span_timestamp.outerHTML ;
 
       document.querySelector('#display-message-section').append(p);
 
